b8a619fcdb923dfbbc11a781cb2631843815670c
Ibrahim-nassih <binni3673@gmail.com>
2024-06-06 20:46:18 +0100

    add front project

---
+++ src/config/env.ts
@@ -0,0 +1,7 @@
+export class Env {
+    public static realm = 'Lead'
+    public static grant_type = 'password'
+    public static client_id = 'AngularPromoteApp'
+    public static client_secret = '126c172f-7bb5-4d70-82f5-c085f83475dc'
+    public static id_innova = '15122dec-b629-46ef-a601-d0af48757a7d'
+}
